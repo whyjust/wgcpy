9d1a96563349b798f59c4784a8398561fb561d4d
whyjust <15858017847@163.com>
2021-07-02 14:33:23 +0800

    feat(__init__.py): add init

---
+++ wgcpy/bins/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+File Name：     __init__.py.py
+Author :        weiguang
+date：          2021/6/18
+Description :
+"""
+__all__ = ['cut_bins', 'chi_merge']
