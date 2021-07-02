cc9e0b14612c2471ec360c19f798218250c84f74
whyjust <15858017847@163.com>
2021-07-02 14:41:43 +0800

    feat(init): add init

---
+++ wgcpy/model/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+File Name：     __init__.py.py
+Author :        weiguang
+date：          2021/6/18
+Description :
+"""
+
+__all__ = ["gen_model", "gen_pmml_model", "dz_eval"]
