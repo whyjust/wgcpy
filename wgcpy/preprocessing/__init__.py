29461622edc806d45023c1d226c02db5b7f32a9a
whyjust <15858017847@163.com>
2021-07-02 14:42:05 +0800

    feat(init): init

---
+++ wgcpy/preprocessing/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+File Name：     __init__.py.py
+Author :        hongyang.w
+date：          2021/6/18
+Description :
+"""
+__all__ = ['eda','data_dectection']
+
